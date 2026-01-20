1a058203c60d8c61cfc973d4833376813a6206fb
Anousheh Hussain <anousheh.hussain@gmail.com>
2026-01-20 19:36:48 +0530

    Ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,11 +15,17 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 const storage = getStorage(app); // <--- INITIALIZE STORAGE
 const provider = new GoogleAuthProvider();
